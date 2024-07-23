126a8a72c25b29699e3dd39b413ca224fd5bacdf
ikucuk <incikucuk98@gmail.com>
2024-07-23 13:50:41 +0300

    first commit

---
+++ src/test/java/com/ikucuk/springboot_blog/SpringbootBlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikucuk.springboot_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
